477a404ecb87e91059a71df1becb12c8ceb962f1
husainb0112 <husainb@nycstudents.net>
2024-10-02 13:32:30 +0000

    v2

--- Main.java
+++ Main.java
@@ -2,9 +2,56 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //Inititialization
         String info;
         Scoreboard game = new Scoreboard("Red", "Blue");
+        
+        //Test 1
+        info = game.getScore();
+        game.recordPlay(1);
+        System.out.println(info);
+
+        //Test 2
+        info = game.getScore();
+        game.recordPlay(0);
+        System.out.println(info);
+
+        //Test 3
         info = game.getScore();
         System.out.println(info);
+        info = game.getScore(); 
+
+        //Test 3
+        game.recordPlay(3);
+        System.out.println(info);
+        info = game.getScore(); 
+        System.out.println(info);
+        game.recordPlay(1);
+        game.recordPlay(0); 
+        
+        //Test 4
+        info = game.getScore(); 
+        System.out.println(info);
+        game.recordPlay(0);
+
+        //Test 5
+        game.recordPlay(4);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+
+        //Test 6
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
+        info = match.getScore(); 
+        System.out.println(info);
+        info = game.getScore();
+        System.out.println(info);
+
+
+        
+
+        
+
+        
     }
 }
--- Scoreboard.java
+++ Scoreboard.java
@@ -15,6 +15,17 @@ public class Scoreboard {
 
     public String getScore()
     {
-        return teamOneScore + '-' + teamTwoScore + '-' + activeTeam;
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+
+    public void recordPlay(int score)
+    {
+        if(teamOne.equals(activeTeam)) teamOneScore += score;
+        if (teamTwo.equals(activeTeam)) teamTwoScore += score;
+        if(score == 0)
+        {
+            if(activeTeam.equals(teamOne)) activeTeam = teamTwo;
+            else if (activeTeam.equals(teamTwo)) activeTeam = teamOne; 
+        }
     }
 }
